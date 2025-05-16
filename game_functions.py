97806961470d2af2ebf86d7fd9878e55949d28fa
vovka021 <vovka021pro@gmail.com>
2025-05-16 18:00:57 +0300

    test 1 rezerv

---
+++ game_functions.py
@@ -0,0 +1,20 @@
+import sys
+import pygame
+def check_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+def update_screen(ai_settings, screen, ship):
+        screen.fill(ai_settings.bg_color)
+        ship.blitme()
+# Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+def check_events(ship):
+     #Обрабатывает нажатия клавиш и события мыши
+     for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                # Переместить корабль вправо.
+                ship.rect.centerx += 10
